e7a5e2e647fa365382f0da2238e5cbfac70e2aa5
chris1howell <chris1howell@msn.com>
2015-11-01 12:19:33 -0800

    3_10_3 Hex
    Created 3.10.3 Hex

---
+++ OpenEVSE_FW_3_10_3/flash.bat
@@ -0,0 +1,8 @@
+@echo off
+avrdude -c USBasp -p m328p -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0x05:m
+avrdude -c USBasp -p m328p -U flash:w:open_evse_3_10_3.hex
+avrdude -c USBasp -p m328p -U eeprom:w:eeprom_24.bin
+echo Last Flash Performed at:
+time /T
+pause
+flash.bat
