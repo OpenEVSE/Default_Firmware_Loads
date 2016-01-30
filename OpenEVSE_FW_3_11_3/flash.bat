ffb5757b10d954c35e74fbb236c282e30cfd3290
chris1howell <chris1howell@msn.com>
2016-01-30 08:40:03 -0800

    Fix for temperature sensors
    Fixed over temperature errors when temperature is negative on the
    MCP9808 and DS3231 Ambient Sensors.

--- OpenEVSE_FW_3_11_3/flash.bat
+++ OpenEVSE_FW_3_11_3/flash.bat
@@ -1,8 +1,8 @@
 @echo off
 avrdude -c USBasp -p m328p -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0x05:m
-avrdude -c USBasp -p m328p -V -U flash:w:open_evse_3_10_3.hex
+avrdude -c USBasp -p m328p -V -U flash:w:open_evse.hex
 avrdude -c USBasp -p m328p -V -U eeprom:w:eeprom_24.bin -V
 echo Last Flash Performed at:
 time /T
 pause
-flash.bat
+
